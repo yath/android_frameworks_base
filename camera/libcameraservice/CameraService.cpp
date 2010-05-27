yath/android_frameworks_base

commit fbb4bf870824a16f9fe5f5397cfc9e7081bfa19e

Camera: add command line option to disable shutter sound at runtime
add provision to disable and enable shutter sound at runtime from
command line.

Change-Id: I038f591ddf44992374112e41230a7fb58c20491d

camera/libcameraservice/CameraService.cpp
@@ -232,8 +232,10 @@ CameraService::Client::Client(const sp<CameraService>& cameraService,
     mHardware->enableMsgType(CAMERA_MSG_ERROR |
                              CAMERA_MSG_ZOOM |
                              CAMERA_MSG_FOCUS);
-
-    mMediaPlayerClick = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
+    property_get("persist.camera.shutter.disable", value, "0");
+    int disableShutterSound = atoi(value);
+    if(disableShutterSound != 1)
+        mMediaPlayerClick = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
     mMediaPlayerBeep = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
     mOverlayW = 0;
     mOverlayH = 0;

commit 50d522eb6e4b21c7d742ee2456183a1ba518fa43

Fix "Camera: add command line option to disable shutter sound at runtime"
This fixes fbb4bf870824a16f9fe5f5397cfc9e7081bfa19e which introduced a
syntax error and was reverted in
4bc3cef76b8b3ea491a86c9b384cbbaafd38398d.

camera/libcameraservice/CameraService.cpp
@@ -216,6 +216,7 @@ CameraService::Client::Client(const sp<CameraService>& cameraService,
         const sp<ICameraClient>& cameraClient, pid_t clientPid)
 {
     int callingPid = getCallingPid();
+    char propVal[PROPERTY_VALUE_MAX];
     LOGD("Client::Client E (pid %d)", callingPid);
     mCameraService = cameraService;
     mCameraClient = cameraClient;
@@ -233,7 +234,9 @@ CameraService::Client::Client(const sp<CameraService>& cameraService,
                              CAMERA_MSG_ZOOM |
                              CAMERA_MSG_FOCUS);
 
-    mMediaPlayerClick = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
+    property_get("persist.camera.shutter.disable", propVal, "0");
+    if(atoi(propVal) != 1)
+        mMediaPlayerClick = newMediaPlayer("/system/media/audio/ui/camera_click.ogg");
     mMediaPlayerBeep = newMediaPlayer("/system/media/audio/ui/VideoRecord.ogg");
     mOverlayW = 0;
     mOverlayH = 0;